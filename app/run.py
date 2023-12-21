eacf61154b9f1dfda34a53ab7252847f10ccf82d
oleh.zorenko <lovemybeard@gmail.com>
2023-12-21 10:45:51 +0200

    Add separate endpoint for setting telegram webhook url

--- app/run.py
+++ app/run.py
@@ -15,27 +15,37 @@ app = FastAPI(
     title="Income tax calculator",
     description="Calculate income tax for United Kingdom.",
     version="0.0.1",
-    docs_url="/api/v1/docs",
+    docs_url="/api/docs",
 )
 
 
 @app.on_event("startup")
 async def on_startup():
     logger.info("On startup things.")
+
+    # Below import is required because handlers for commands has to be loaded.
+    # Otherwise, sending commands will result in nothing.
+    import app.handlers  # noqa: F401
+
+
+@app.post('/set_webhook')
+async def set_webhook():
+    logger.info(f"app_base_url={settings.app_base_url}.")  # TODO Remove
+    logger.info(f"telegram_webhook_url={settings.telegram_webhook_url()}.")  # TODO Remove
+
     webhook_info = await bot.get_webhook_info()
     logger.info(f"webhook_info: {webhook_info}.")
 
     webhook_url = settings.telegram_webhook_url()
     if webhook_info.url != webhook_url:
         assert (
-            await bot.set_webhook(url=webhook_url) is True
+                await bot.set_webhook(url=webhook_url) is True
         ), "Result of `set_webhook` has to be `True`."
         webhook_info = await bot.get_webhook_info()
         logger.info(f"webhook_info updated: {webhook_info}.")
+        return {'new': webhook_url}
 
-    # Below import is required because handlers for commands has to be loaded.
-    # Otherwise, sending commands will result in nothing.
-    import app.handlers  # noqa: F401
+    return {'existing': webhook_info.url}
 
 
 @app.post(settings.telegram_webhook_path(), include_in_schema=False)
